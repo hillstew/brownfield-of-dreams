abee97b7e973b125346aa27951b932b522edfdc8
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-10 15:06:27 -0700

    Add associations to User model and methods
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def friends_with?(gh_user)
+    friend = User.find_by(gh_id: gh_user.gh_id)
+    friends = friendships.where(friend_id: friend.id)
+    if friends.empty?
+      false
+    else
+      true
+    end
+  end
 end
