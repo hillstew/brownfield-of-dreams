43c5f307545b60a6c387ba4f6c089e16b2646124
Ryan Hantak <rhantak@gmail.com>
2019-12-12 11:07:51 -0700

    Reformat test to match hash

--- spec/models/youtube_video_spec.rb
+++ spec/models/youtube_video_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe YouTube::Video do
   it "is initialized with a thumbnail" do
-    data = { items: { snippet: { thumbnails: { high: {url: 'test_url'}}}}}
+    data = { items: [{ snippet: { thumbnails: { high: {url: 'test_url'}}}}]}
     YouTube::Video.new(data)
   end
 end
